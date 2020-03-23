9b5a35c91d7e0e67e18470963fdc6d33f0802080
andybir <abirosak@gmail.com>
2020-03-23 12:34:32 -0400

    added custom auth for user login

--- parent_helper/settings.py
+++ parent_helper/settings.py
@@ -102,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
