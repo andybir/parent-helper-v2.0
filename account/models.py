fba6349befff9038df2c0ce249581170b8bd7d0f
andybir <abirosak@gmail.com>
2020-03-23 09:48:29 -0400

    post, comment models migrated

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d',
+                              blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
--- parent_helper/settings.py
+++ parent_helper/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'topic.apps.TopicConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
