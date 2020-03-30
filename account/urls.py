e184d562ab8f4ae950d004e080953c7b35eb7074
andybir <abirosak@gmail.com>
2020-03-30 15:50:10 -0400

    changed logout view to render logged out template and header correctly

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # post views
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LoginView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
 
     # change password urls
